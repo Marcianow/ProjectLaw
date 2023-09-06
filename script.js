879bae9b89800d4ae6e4d7b5bae1445218d34bdc
Bruno <brunomarcianosantos@usp.br>
2023-09-05 21:38:09 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function eventManager(){
+    console.log("Ola");
+    var areas = document.getElementsByClassName("card");
+    for(var i = 0; i < areas.length; i++) {
+        (function(index) {
+            areas[index].addEventListener("click", function() {
+                var anchor = document.createElement('a');
+                anchor.href = 'https://web.whatsapp.com/send?phone=+5511995302558';
+                anchor.target="_blank";
+                anchor.click();
+                //window.location.href = '';
+           })
+        })(i);
+      }
+}
+
+
+document.addEventListener("DOMContentLoaded", eventManager, false);
+
